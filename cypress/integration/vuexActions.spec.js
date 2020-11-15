e9ebcac935ed92a97a713d24c4ea1216127bc9e8
glennpacker <glenn.packer@branthill.co.uk>
2020-11-15 10:27:50 +0000

    vuex getters snippets moved to service, vuex implementation with e2e and unit tests

--- cypress/integration/vuexActions.spec.js
+++ cypress/integration/vuexActions.spec.js
@@ -1,4 +1,4 @@
-describe('vuex setup page', () => {
+describe('Vuex Actions page', () => {
     it('should render with a title', () => {
         cy.visit(`${ Cypress.env().url }vuexactions`);
         cy.get('h3').contains('Vuex - Actions');
